f327c620ea7765ace7d225822ea2809e9c2379eb
ChiantiWine <hsm11256@naver.com>
2025-02-13 20:49:50 +0900

    2025.02.13
    1. Enemy - Animation
    2. Weapon - Efect(Particle), Hit(Particle), Animation

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,11 +1,16 @@
 using StarterAssets;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     StarterAssetsInputs starterAssetsInputs;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitVFXPrefab;
+    [SerializeField] Animator animator;
     [SerializeField] int damagerAmount = 1;
 
+    const string SHOOT_STRING = "Shoot";
     void Awake()
     {
         starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
@@ -23,19 +28,22 @@ public class Weapon : MonoBehaviour
     void HandleShoot()
     {
         if (!starterAssetsInputs.shoot) return;
-        
+
+        muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
+
         RaycastHit hit;
 
                 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
+            Instantiate(hitVFXPrefab, hit.point, quaternion.identity);
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(damagerAmount); // ? null 조건부 연산자
-
         }
 
-        starterAssetsInputs.ShootInput(false);
-
+       
     }
 
 }
