c1c9d7b0a4fa2724edd127a69f97ecda5b94fe4c
ChiantiWine <hsm11256@naver.com>
2025-02-13 18:04:10 +0900

    2025.02.13
    1. enenmy Health
    2. Weapon Damage

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,7 +1,15 @@
+using StarterAssets;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    StarterAssetsInputs starterAssetsInputs;
+    [SerializeField] int damagerAmount = 1;
+
+    void Awake()
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+    }
     void Start()
     {
         
@@ -9,12 +17,25 @@ public class Weapon : MonoBehaviour
 
     void Update()
     {
+        HandleShoot();
+    }
+
+    void HandleShoot()
+    {
+        if (!starterAssetsInputs.shoot) return;
+        
         RaycastHit hit;
 
-        
+                
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
-        Debug.Log(hit.collider.name);
+            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+            enemyHealth?.TakeDamage(damagerAmount); // ? null 조건부 연산자
+
         }
+
+        starterAssetsInputs.ShootInput(false);
+
     }
+
 }
